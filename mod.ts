2f1c7b23d094b6535ce9fa16e4772f3c374f3290
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2022-05-02 00:00:41 +0200

    Bump version
    Solves 400 webhook response

--- mod.ts
+++ mod.ts
@@ -2,7 +2,7 @@
 
 /**
  *
- * 	v0.0.2
+ * 	v0.0.3
  *
  * 	Nano — a very simple (semi) logic-less template engine. This was initially
  * 	made for playing around with simple prototypes deployed with Deno Deploy,
