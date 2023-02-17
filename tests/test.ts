ccefd0cc02e8c9315dbe86a514f5033c94b196ba
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2023-02-17 21:06:39 +0100

    Update test types

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,4 @@
-import type { RootTemplate } from '../src/types.ts';
+import type { Template } from '../src/types.ts';
 import { Parse } from '../src/parser.ts';
 import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
 
@@ -7,13 +7,22 @@ const tests = [
 		name: 'if else',
 		expression: `{if this} A {else} B {/if}`,
 		result: {
-			type: 'RootTemplate',
+			type: 'Template',
 			value: [
 				{
 					type: 'If',
-					test: 'if this',
+					statement: {
+						type: 'IfStatement',
+						test: {
+							type: 'Identifier',
+							value: 'this',
+						},
+					},
 					consequent: [{ type: 'Text', value: ' A ' }],
-					alternate: { type: 'Else', value: [{ type: 'Text', value: ' B ' }] },
+					alternate: {
+						type: 'Else',
+						value: [{ type: 'Text', value: ' B ' }],
+					},
 				},
 			],
 		},
@@ -22,6 +31,6 @@ const tests = [
 
 for (const test of tests) {
 	Deno.test('>> ' + test.name, () => {
-		assertEquals(Parse(test.expression), test.result as Root);
+		assertEquals(Parse(test.expression), test.result as Template);
 	});
 }
