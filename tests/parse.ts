e90d51595c7389dc1c5daf07ec7f8bc2538157bc
Alejandro V. Rojas <6848068+alevroub@users.noreply.github.com>
2023-02-22 19:41:10 +0100

    Add timer

--- tests/parse.ts
+++ tests/parse.ts
@@ -2,7 +2,9 @@ import { parse } from '../src/parser.ts';
 
 try {
 	const input = Deno.readTextFileSync('tests/input.html');
+	console.time('parse');
 	const parsed = parse(input);
+	console.timeEnd('parse');
 
 	console.dir(parsed, { depth: 10 });
 } catch (error) {
