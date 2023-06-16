b4f99b266bcef9c194d52a226c10eb728447d4e7
Alejandro V. Rojas <6848068+alejandrovrojas@users.noreply.github.com>
2023-06-16 17:08:03 +0200

    Use performance object for benchmarking

--- tests/test_print_parsed.ts
+++ tests/test_print_parsed.ts
@@ -2,10 +2,12 @@ import { parse } from '../src/parser.ts';
 
 try {
 	const input_template = Deno.readTextFileSync('tests/input_template.html');
-	console.time('parse');
-	const parsed = parse(input_template);
-	console.timeEnd('parse');
 
+	const time_start = performance.now();
+	const parsed = parse(input_template);
+	const time_end = performance.now();
+
+	console.log(`%cPARSE ${time_end - time_start}ms`, 'color: red;');
 	console.dir(parsed, { depth: 10 });
 } catch (error) {
 	console.log(error);
--- tests/test_print_rendered.ts
+++ tests/test_print_rendered.ts
@@ -1,13 +1,14 @@
 import { render } from '../src/renderer.ts';
-import { input_data } from './input_data.js';
+import { input_data } from './input_data.ts';
 
 try {
 	const input_template = Deno.readTextFileSync('tests/input_template.html');
 
-	console.time('render');
+	const time_start = performance.now();
 	const rendered = await render(input_template, input_data);
-	console.timeEnd('render');
+	const time_end = performance.now();
 
+	console.log(`%cRENDER ${time_end - time_start}ms`, 'color: red;');
 	console.dir(rendered, { depth: 10 });
 } catch (error) {
 	console.log(error);
